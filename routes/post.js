5adcec9aec528748c4e2455a3a725ca6c54cf19e
Paris Taylor <paristechprofile@gmail.com>
2019-03-05 12:11:12 -0800

    adds the post route

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,11 @@ const
     router = express.Router(),
     controllers = require('../controllers')
 
-router.get('/', controllers.post.index)
+//get all posts by users. will show on profile page
+router.get('/user/:id/posts', controllers.post.findAllByUser);
+router.get('/cities/:id/posts', controllers.post.findAllByCity);
+router.post('/cities/:id/user/:id/post', controllers.post.createPost);
+router.put('/post/:id', controllers.post.editPost);
+router.delete('/post/:id', controllers.post.deletePost);
 
 module.exports = router;
