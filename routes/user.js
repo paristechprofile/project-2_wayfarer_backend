1586d18630a090cb64039323b798c5fd31b69bb0
Paris Taylor <paristechprofile@gmail.com>
2019-03-05 11:57:29 -0800

    adds user routes. reviewed with team

--- routes/user.js
+++ routes/user.js
@@ -3,26 +3,40 @@ const
     router = express.Router(),
     jwt = require('jsonwebtoken'),
     controllers = require('../controllers');
+    
+//get all users
+router.get('/users', controllers.user.findAll)
 
-    router.use((req, res, next) => {
-        console.log('user.js route activated router.use')
-        const bearerHeader = req.headers['authorization'];
-        console.log('user.js triggered token check', bearerHeader)
+//get one user
+router.get('/', controllers.user.findById)
 
-        if(typeof bearerHeader !== 'undefined'){
-            const bearer = bearerHeader.split(' ');
-            const bearerToken = bearer[1];
-            req.token = bearerToken; 
-            let verified = jwt.verify(req.token, 'fantastic4');
-            console.log('here is the verified', verified)
-            req.userId = verified._id
-            next();
-        }
-        else {
-            res.sendStatus(403);
-        }
-    })
+//create user
+router.post('/signup', controllers.user.signup)
 
-    router.get('/', controllers.user.show)
+//edit user profile
+router.put('/user/:id', controllers.user.editProfile)
 
-    module.exports = router;
+//delete user
+router.delete('/user/:id', controllers.user.delete)
+
+//middleware to add token to request/response
+router.use((req, res, next) => {
+    console.log('user.js route activated router.use')
+    const bearerHeader = req.headers['authorization'];
+    console.log('user.js triggered token check', bearerHeader)
+
+    if(typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken; 
+        let verified = jwt.verify(req.token, 'fantastic4');
+        console.log('here is the verified', verified)
+        req.userId = verified._id
+        next();
+    }
+    else {
+        res.sendStatus(403);
+    }
+})
+
+module.exports = router;
