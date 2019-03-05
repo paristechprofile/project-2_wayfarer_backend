d5688c6fe6bf06e2c4852875e35ee485ad47d223
Paris Taylor <paristechprofile@gmail.com>
2019-03-05 11:26:26 -0800

    finished reviewing models with the team. begins route review with team

--- models/city.js
+++ models/city.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const CitySchema = new mongoose.Schema({
-
+const CitySchema = new Schema({
+    name: String,
+    state: String,
+    country: String,
+    image: String
 })
 
 module.exports = mongoose.model('City', CitySchema)
