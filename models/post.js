2c409a40e4607a14787359c22fd67bacda3477ab
Paris Taylor <paristechprofile@gmail.com>
2019-03-05 11:22:35 -0800

    made changes to models. Reviewing files with team

--- models/post.js
+++ models/post.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
-    
+    date: Date,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    city: {
+        type: Schema.Types.ObjectId,
+        ref: 'City' 
+    },
+    text: String
 })
 
 module.exports = mongoose.model('Post', PostSchema)
--- models/user.js
+++ models/user.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    pw: {
+        type: String,
+        require: true,
+        select: false 
+    },
+    image: String,
+    firstName: String,
+    lastName: String
 })
 
 module.exports = mongoose.model('User', UserSchema)
