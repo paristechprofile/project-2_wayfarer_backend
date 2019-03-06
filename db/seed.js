335920af8f3ee24f8d13aa708d184924ab4a551e
nassBouz <bouzianenassima@gmail.com>
2019-03-05 23:11:27 -0800

    created functionality to seed the data from city.json , user.json, post.json

--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,76 @@
 const 
-    data = require('./user'),
-    db = require('../models')
+    data = require('./user'), // we may need to change data to user
+    db = require('../models'),
+    city = require('./city'),
+    post = require('./post')
 
 
-db.User.remove({}, (err) =>{
-    db.User.create(data, (err, users) =>{
-        if (err) {
-            console.log('cannot create user', err)
-        } console.log(users)
-    })
-});
+// db.User.remove({}, (err) =>{
+//     db.User.create(data, (err, users) =>{
+//         if (err) {
+//             console.log('cannot create user', err)
+//         } console.log(users)
+//     })
+// });
+
+// db.City.remove({}, (err) =>{
+//     db.City.create(city, (err, cities) =>{
+//         if (err) {
+//             console.log('cannot create city', err)
+//         } console.log(cities)
+//     })
+// });
+
+
+db.City.deleteMany({}, (err, cities) => {
+    console.log('remove all cities');
+    
+    //create the cities first
+    db.City.create(city, (err, cities) => {
+      if(err) { console.log(err); }
+      console.log('created all cities');
+      console.log(`created ${cities.length} cities`);
+    
+      /// create the users
+      db.User.deleteMany({},(err, users) => {
+        if (err) { console.log('Error occurred in removing users', err) } 
+        console.log('removed all users from the users list');
+        
+        db.User.create(data, function(err, users){
+          if (err) { return console.log('err', err) }
+          console.log("created", users.length, "users");
+          console.log(users);
+  
+          db.Post.deleteMany({}, (err, posts)=>{
+            if(err) { console.log('Error occurred in removing posts', err) } 
+            console.log('removed all posts');
+        
+            post.forEach(postData => {
+              let newPost = new db.Post({
+                text: postData.text,
+                date: postData.date
+              });  
+
+              // look for city
+              db.City.findOne({ name: postData.city}, (err, foundCity)=>{
+                if (err) { console.log(err) }
+                newPost.city = foundCity;
+        
+                /// look for user
+                db.User.findOne({ username: postData.author}, (err, foundUser) => {
+                  if (err) { console.log(err) }
+                  newPost.author = foundUser;
+        
+                  newPost.save((err, savedPost) => {
+                    if (err) { console.log(err) }
+                    console.log(savedPost);
+                    console.log('saved!!!');
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
