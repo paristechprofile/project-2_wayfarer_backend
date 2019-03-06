54d982be5998664ec8683462bf983f42b5f52aed
nassBouz <bouzianenassima@gmail.com>
2019-03-06 11:42:25 -0800

    difined a route for cities

--- routes/cities.js
+++ routes/cities.js
@@ -3,10 +3,10 @@ const
     router = express.Router(),
     controllers = require('../controllers')
 
-// router.get('/cities', controllers.cities.findAll);
-// router.get('/cities/:id', controllers.cities.findById);
-// router.post('/cities', controllers.cities.create);
-// router.put('/cities/:id', controllers.cities.edit);
-// router.delete('/cities/:id', controllers.cities.delete);
+router.get('/', controllers.cities.findAll);
+// router.get('/:id', controllers.cities.findById);
+// router.post('/', controllers.cities.create);
+// router.put('/:id', controllers.cities.edit);
+// router.delete('/:id', controllers.cities.delete);
 
 module.exports = router;
