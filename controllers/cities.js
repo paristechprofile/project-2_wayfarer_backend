61f1461eab07977b29ae267326080fc92839b414
nassBouz <bouzianenassima@gmail.com>
2019-03-06 14:53:36 -0800

    defined more routes for cities

--- controllers/cities.js
+++ controllers/cities.js
@@ -10,7 +10,48 @@ module.exports = {
             } res.json(foundCities)
         })
     },
+    editCity: (req,res)=>{
+        console.log('update city', req.params);
+        console.log(`the body is${req.body}`);
+        const cityId = req.params.id;
+        db.City.findOneAndUpdate(
+            {_id:cityId},
+            req.body,
+            // {name: req.params.name},
+            {new: true},
+            (err, updateCity) => {
+                if(err) {throw err;}
+                res.json(updateCity);
+        });
+        //////////////////////
+    },
+    findById: (req, res)=>{
+        db.City.find({_id: req.params.id}, (error, cities) => {
+            res.json(cities);
+          }); 
+
+    },
+    createCity: (req,res)=>{
+        let newCity = new db.City({
+            name: req.body.name,
+            country: req.body.country,
+            state: req.body.state,
+            image: req.body.image
+          });
+          newCity.save((error, city) => {
+            res.json(city);
+          });
+    },
+    deleteCity: (req, res)=>{
+        const cityId = req.params.id;
+        console.log('delete user ', cityId);
+        db.City.findOneAndDelete({_id: cityId}, (err, deletedCity) => {
+            if(err) { throw err; }
+            res.json(deletedCity);
+          });
+    }
 }
+
 
 // router.get('/cities', controllers.cities.findAll);
 
