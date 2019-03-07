28b4adba12aae2a07c1c500717d714680d71c327
nassBouz <bouzianenassima@gmail.com>
2019-03-07 11:28:55 -0800

    create a post

--- controllers/post.js
+++ controllers/post.js
@@ -15,48 +15,11 @@ module.exports = {
         res.json(foundPosts)
     })
   },
+  
+    
 
   }
-// editCity: (req,res)=>{
-//   console.log('update city', req.params);
-//   console.log(`the body is${req.body}`);
-//   const cityId = req.params.id;
-//   db.City.findOneAndUpdate(
-//       {_id:cityId},
-//       req.body,
-//       // {name: req.params.name},
-//       {new: true},
-//       (err, updateCity) => {
-//           if(err) {throw err;}
-//           res.json(updateCity);
-//   });
-//   //////////////////////
-// },
-// findById: (req, res)=>{
-//   db.City.find({_id: req.params.id}, (error, cities) => {
-//       res.json(cities);
-//     }); 
 
-// },
-// createCity: (req,res)=>{
-//   let newCity = new db.City({
-//       name: req.body.name,
-//       country: req.body.country,
-//       state: req.body.state,
-//       image: req.body.image
-//     });
-//     newCity.save((error, city) => {
-//       res.json(city);
-//     });
-// },
-// deleteCity: (req, res)=>{
-//   const cityId = req.params.id;
-//   console.log('delete user ', cityId);
-//   db.City.findOneAndDelete({_id: cityId}, (err, deletedCity) => {
-//       if(err) { throw err; }
-//       res.json(deletedCity);
-//     });
-// }
 // router.get('/user/:id/posts', controllers.post.findAllByUser);
 // router.get('/cities/:id/posts', controllers.post.findAllByCity);
 
