6118caa682c42c7e28fbb42f82626a7a8dace0a6
nassBouz <bouzianenassima@gmail.com>
2019-03-06 16:17:33 -0800

    /user

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'))
 
-app.use('/', userRoutes)
+app.use('/user', userRoutes)
 app.use('/cities', citiesRoutes)
 app.use('/post', postRoutes)
 
