fbf674380e19347e4bf5e0bdea82ca0432998801
nassBouz <bouzianenassima@gmail.com>
2019-03-07 11:30:48 -0800

    added the midleware to decrypt the user id

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,41 @@
 const 
     express = require('express'),
     router = express.Router(),
+    jwt = require('jsonwebtoken'),
     controllers = require('../controllers')
+    
 
 // //get all posts by users. will show on profile page
- router.get('/', controllers.post.findAllPostsByUser);
+
+ router.use((req, res, next) => {
+    console.log('user.js route activated router.use')
+    const bearerHeader = req.headers['authorization'];
+    console.log('user.js triggered token check', bearerHeader)
+
+    if(typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken; 
+        let verified = jwt.verify(req.token, 'fantastic4');
+        console.log('here is the verified', verified)
+        req.userId = verified._id
+        console.log(req.userId)
+        next();
+    }
+    else {
+        res.sendStatus(403);
+    }
+});
+
+router.get('/', controllers.post.findAllPostsByUser);
+
+
+
+
+
  //.get('/',controllers.post.findAllPostsByCity)
 // router.get('/cities/:id/posts', controllers.post.findAllByCity);
-// router.post('/cities/:id/user/:id/post', controllers.post.createPost);
+//  router.post('/cities/:id/post', controllers.post.createPost);
 // router.put('/post/:id', controllers.post.editPost);
 // router.delete('/post/:id', controllers.post.deletePost);
 
