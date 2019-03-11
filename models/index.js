d417ccbe42f5943be83b1ecd507a0e0815199797
Paris Taylor <paristechprofile@gmail.com>
2019-03-10 19:00:07 -0700

    trying to push to heroku

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/wayfarer', { useNewUrlParser: true })
 
 module.exports = {
-    City: require('./City'),
-    Post: require('./Post'),
-    User: require('./User')
+    City: require('./City.js'),
+    Post: require('./Post.js'),
+    User: require('./User.js')
 }
