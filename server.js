34e1a6461209dabb7fa54ee4237b90f96338879d
Paris Taylor <paristechprofile@gmail.com>
2019-03-11 09:17:29 -0700

    issues with pushing

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const
     app = express();
 
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'))
