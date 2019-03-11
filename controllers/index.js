75bf73b488c478740c0f1e98b90a67c5a2917d1e
Paris Taylor <paristechprofile@gmail.com>
2019-03-10 17:47:23 -0700

    pushing to heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true } );
+
 module.exports = {
     user: require('./user'),
     cities: require('./cities'),
